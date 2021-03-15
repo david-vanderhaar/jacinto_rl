9ab4b2a2b4ade7a4e4cde492986ffa41f951b67a
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2021-03-14 21:44:24 -0400

    Updates

---
+++ precache-manifest.1798c566754f4c7387c008ec666d2fd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9fc21d3fe6df57c06d362746c2dc0ef",
+    "url": "./index.html"
+  },
+  {
+    "revision": "774a67ecae60716d54ee",
+    "url": "./static/css/main.86e29578.chunk.css"
+  },
+  {
+    "revision": "307c634021e52a190c44",
+    "url": "./static/js/2.d20896ee.chunk.js"
+  },
+  {
+    "revision": "774a67ecae60716d54ee",
+    "url": "./static/js/main.5fc829c7.chunk.js"
+  },
+  {
+    "revision": "0b7e4e173982279802e6",
+    "url": "./static/js/runtime~main.8707be79.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ea12c348c1a14c1ea0aac5a7b445c963.js"
+  "./precache-manifest.1798c566754f4c7387c008ec666d2fd9.js"
 );
 
 self.addEventListener('message', (event) => {
