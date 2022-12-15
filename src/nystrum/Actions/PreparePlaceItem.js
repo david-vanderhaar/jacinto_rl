8559cc6a100b5a5c5d3d70ffde1bd9511e822527
David Vanderhaar <d.vanderhaarhunter@gmail.com>
2022-12-14 20:37:41 -0500

    WIP: abstracting place item;

---
+++ src/nystrum/Actions/PreparePlaceItem.js
@@ -0,0 +1,80 @@
+import { Base } from './Base';
+import { PlaceItem } from './PlaceItem';
+import { GoToPreviousKeymap } from './GoToPreviousKeymap';
+import { DIRECTIONS, ENERGY_THRESHOLD } from '../constants';
+import { getPositionInDirection } from '../../helper';
+
+const directionKeys = [
+  'N',
+  'S',
+  'E',
+  'W',
+]
+
+const directions = directionKeys.map((key) => DIRECTIONS[key])
+
+const directionKeyMap = {
+  N: 'w',
+  S: 's',
+  E: 'd',
+  W: 'a',
+}
+
+export class PreparePlaceItem extends Base {
+  constructor({
+    passThroughEnergyCost = ENERGY_THRESHOLD, 
+    passThroughRequiredResources = [],
+    entity,
+    ...args 
+  }) {
+    super({ ...args });
+    this.passThroughEnergyCost = passThroughEnergyCost;
+    this.passThroughRequiredResources = passThroughRequiredResources;
+    this.processDelay = 0;
+    this.energyCost = 0;
+  }
+  perform() {
+    const pos = this.actor.getPosition();
+    const cursor_positions = directions.map((direction) => getPositionInDirection(pos, direction));
+    this.actor.activateCursor(cursor_positions)
+
+    const goToPreviousKeymap = new GoToPreviousKeymap({
+      actor: this.actor,
+      game: this.game,
+      onAfter: () => this.actor.deactivateCursor(),
+    })
+
+
+    let keymap = {
+      Escape: () => goToPreviousKeymap,
+    };
+
+
+    directionKeys.forEach((key) => {
+      const action = () => { 
+        return new PlaceItem({
+          actor: this.actor,
+          game: this.game,
+          interrupt: false,
+          energyCost: this.passThroughEnergyCost,
+          requiredResources: this.passThroughRequiredResources,
+          label: `target ${key}`,
+          entity,
+          targetPos: getPositionInDirection(pos, DIRECTIONS[key]),
+          onSuccess: () => {
+            this.actor.deactivateCursor();
+            this.actor.setNextAction(goToPreviousKeymap);
+          },
+        })
+      }
+
+      keymap[directionKeyMap[key]] = action
+    })
+
+    this.actor.setKeymap(keymap);
+    return {
+      success: true,
+      alternative: null,
+    };
+  }
+};
